107e3eab8af4342d206e506ba0202ca6d9a83076
mzabsky@gmail.com <mzabsky@gmail.com@8f1d2d58-461c-b729-9895-e035e2c52f4a>
2010-12-30 17:03:23 +0000

    Initial commit (version 1.0.3).

---
+++ trunk/build-debug.bat
@@ -0,0 +1,29 @@
+@echo off
+del output\RGH.pk3
+mkdir temp
+mkdir temp\acs
+mkdir temp\acs\acs
+mkdir output
+utility\acc "ACS source\RGH_ACS.acs" temp\acs\acs\RGH_ACS
+
+move temp\acs\acs\RGH_ACS.o temp\acs\acs\RGH_ACS
+
+if exist "acs.err" goto acserror
+
+cd data\monsters
+..\..\utility\7z u ..\..\temp\debug-monsters.pk3 * -mx0
+cd ..
+..\utility\7z a ..\temp\debug-acs.pk3 LOADACS -mx0
+cd ..\temp\acs
+..\..\utility\7z a ..\..\temp\debug-acs.pk3 acs\RGH_ACS -mx0
+cd ..\..
+skulltag data/core/* temp/debug-acs.pk3 temp/debug-monsters.pk3
+exit
+
+:acserror
+	echo ==============================================================
+	type acs.err
+	move acs.err acs_err.log
+	echo ==============================================================
+	del acs.err
+	pause
