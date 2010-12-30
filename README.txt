6bf54ec00328023a059afdff2ba44a4baca86685
mzabsky <mzabsky@gmail.com>
2010-12-30 17:03:23 +0000

    Initial commit (version 1.0.3).

---
+++ README.txt
@@ -0,0 +1,7 @@
+Real Guns Hardcode
+
+Building release version:
+Just execute the build-release.bat file. The WAD file will be in the output folder once the console window closes.
+
+Debugging:
+Since the build-release.bat takes a lot of time to execute, there is also a build-debug.bat file. To use this, you have to put your skulltag directory to the system PATH variable (google how to do this). Then you can execute the build-debug.bat and Skulltag with loaded debug version of RGH will open within moments.
---
+++ build-release.bat
@@ -0,0 +1,32 @@
+@echo off
+del output\RGH.pk3
+mkdir temp
+mkdir temp\acs
+mkdir temp\acs\acs
+mkdir output
+utility\acc "ACS source\RGH_ACS.acs" temp\acs\acs\RGH_ACS
+
+move temp\acs\acs\RGH_ACS.o temp\acs\acs\RGH_ACS
+
+if exist "acs.err" goto acserror
+
+cd data
+..\utility\7z a ..\output\RGH.pk3 credits.txt
+cd core
+..\..\utility\7z a ..\..\output\RGH.pk3 *
+cd ..\monsters
+..\..\utility\7z a ..\..\output\RGH.pk3 *
+cd ..
+..\utility\7z a ..\output\RGH.pk3 LOADACS
+cd ..\temp\acs
+..\..\utility\7z a ..\..\output\RGH.pk3 acs\RGH_ACS
+cd ..\..
+exit
+
+:acserror
+	echo ==============================================================
+	type acs.err
+	move acs.err acs_err.log
+	echo ==============================================================
+	del acs.err
+	pause
