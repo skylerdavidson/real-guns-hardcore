4eb2357106cbec022403d6f2aea9277ea9f908a3
mzabsky@gmail.com <mzabsky@gmail.com@8f1d2d58-461c-b729-9895-e035e2c52f4a>
2011-03-14 22:26:53 +0000

    Added support for ZDoom.

---
+++ trunk/build-debug-skulltag.bat
@@ -0,0 +1,28 @@
+@echo off
+del output\RGH.pk3
+mkdir temp
+mkdir temp\acs
+mkdir temp\acs\acs
+mkdir output
+utility\mcpp "ACS source\RGH_ACS.acs" -o temp\acs\processed.acs -D SKULLTAG -D IgnoreHash(x)=x -P
+utility\acc temp\acs\processed.acs temp\acs\acs\RGH_ACS
+
+move temp\acs\acs\RGH_ACS.o temp\acs\acs\RGH_ACS
+
+if exist "acs.err" goto acserror
+
+cd data
+..\utility\7z u ..\temp\RGH-debug-skulltag.pk3 * -xr!.svn  -mx0
+cd ..\temp\acs
+..\..\utility\7z u ..\..\temp\RGH-debug-skulltag.pk3 acs\RGH_ACS -mx0
+cd ..\..
+skulltag temp/RGH-debug-skulltag.pk3
+exit
+
+:acserror
+	echo ==============================================================
+	type acs.err
+	move acs.err acs_err.log
+	echo ==============================================================
+	del acs.err
+	pause
