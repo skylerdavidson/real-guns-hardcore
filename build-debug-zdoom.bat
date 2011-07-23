c190ce07a00c6f6b52a50ed30ca8995d469c278d
mzabsky <mzabsky@gmail.com>
2011-07-23 20:05:00 +0000

    Improved batch files (patch by Edward-san).

--- build-debug-zdoom.bat
+++ build-debug-zdoom.bat
@@ -1,30 +1,29 @@
 @echo off
-mkdir temp
-mkdir temp\acs
-mkdir temp\acs\acs
-mkdir output
+if not exist "temp" mkdir temp
+if not exist "temp\acs" mkdir temp\acs
+if not exist "temp\acs\acs" mkdir temp\acs\acs
+if not exist "output" mkdir output
 utility\mcpp "ACS source\RGH_ACS.acs" -o temp\acs\processed.acs -D ZDOOM -D IgnoreHash(x)=x -D DEBUG -P
 echo ==============================================================
 utility\zmp -d data/decorate -a "temp\acs\processed.acs" -o temp\acs\zmp_processed.acs -p data/decorate.txt -m skulltag
 echo ==============================================================
 utility\acc temp\acs\zmp_processed.acs temp\acs\acs\RGH_ACS
 
-if exist "temp\acs\acs.err" goto acserror
+if exist "temp\acs\acs.err" (
+    echo ==============================================================
+    move /y temp\acs\acs.err acs_errors.log	
+    pause
+) else (
+    move /y temp\acs\acs\RGH_ACS.o temp\acs\acs\RGH_ACS
 
-move temp\acs\acs\RGH_ACS.o temp\acs\acs\RGH_ACS
-
-cd data
-..\utility\7z u ..\temp\RGH-debug-zdoom.pk3 * -xr!.svn  -mx0
-cd ..\temp\acs
-..\..\utility\7z u ..\..\temp\RGH-debug-zdoom.pk3 acs\RGH_ACS -mx0
-cd ..\..
-cd "zdoom data"
-..\utility\7z u ..\temp\RGH-debug-zdoom.pk3 * -xr!.svn  -mx0
-cd ..
-zdoom temp/RGH-debug-zdoom.pk3
-exit
-
-:acserror
-	echo ==============================================================
-	move temp\acs\acs.err acs_errors.log	
-	pause
+    cd data
+    ..\utility\7z u ..\temp\RGH-debug-zdoom.pk3 * -xr!.svn  -mx0
+    cd ..\temp\acs
+    ..\..\utility\7z u ..\..\temp\RGH-debug-zdoom.pk3 acs\RGH_ACS -mx0
+    cd ..\..
+    cd "zdoom data"
+    ..\utility\7z u ..\temp\RGH-debug-zdoom.pk3 * -xr!.svn  -mx0
+    cd ..
+    zdoom temp/RGH-debug-zdoom.pk3
+    exit
+)
--- build-release.bat
+++ build-release.bat
@@ -1,40 +1,41 @@
 @echo off
-del output\RGH-Skulltag.pk3
-del output\RGH-ZDoom.pk3
-mkdir temp
-mkdir temp\acs
-mkdir temp\acs-skulltag\acs
-mkdir temp\acs-zdoom\acs
-mkdir output
+if exist "output\RGH-Skulltag.pk3" del output\RGH-Skulltag.pk3
+if exist "output\RGH-Skulltag.pk3" del output\RGH-ZDoom.pk3
+if not exist "temp" mkdir temp
+if not exist "temp\acs" mkdir temp\acs
+if not exist "temp\acs-skulltag\acs" mkdir temp\acs-skulltag\acs
+if not exist "temp\acs-zdoom\acs" mkdir temp\acs-zdoom\acs
+if not exist "output" mkdir output
 echo ==============================================================
 utility\mcpp "ACS source\RGH_ACS.acs" -o temp\acs-skulltag\processed.acs -D SKULLTAG -D IgnoreHash(x)=x -P
 utility\acc temp\acs-skulltag\processed.acs temp\acs-skulltag\acs\RGH_ACS
-if exist "temp\acs-skulltag\acs.err" goto acserror
+if exist "temp\acs-skulltag\acs.err" (
+    echo ==============================================================
+    move /y temp\acs-skulltag\acs.err acs_errors_skulltag.log
+    pause
+) else (
+    echo ==============================================================
+    utility\mcpp "ACS source\RGH_ACS.acs" -o temp\acs-zdoom\processed.acs -D ZDOOM -D IgnoreHash(x)=x -P
+    utility\acc temp\acs-zdoom\processed.acs temp\acs-zdoom\acs\RGH_ACS
+    if exist "temp\acs-zdoom\acs.err" (
+        echo ==============================================================
+        move /y temp\acs-zdoom\acs.err acs_errors_zdoom.log	
+        pause
+    ) else (
+        if exist 
+        move /y temp\acs-skulltag\acs\RGH_ACS.o temp\acs-skulltag\acs\RGH_ACS
+        move /y temp\acs-zdoom\acs\RGH_ACS.o temp\acs-zdoom\acs\RGH_ACS
 
-echo ==============================================================
-utility\mcpp "ACS source\RGH_ACS.acs" -o temp\acs-zdoom\processed.acs -D ZDOOM -D IgnoreHash(x)=x -P
-utility\acc temp\acs-zdoom\processed.acs temp\acs-zdoom\acs\RGH_ACS
-if exist "temp\acs-zdoom\acs.err" goto acserror
-
-move temp\acs-skulltag\acs\RGH_ACS.o temp\acs-skulltag\acs\RGH_ACS
-move temp\acs-zdoom\acs\RGH_ACS.o temp\acs-zdoom\acs\RGH_ACS
-
-
-
-cd data
-..\utility\7z a ..\output\RGH-Skulltag.pk3 * -xr!.svn -mx0
-copy ..\output\RGH-Skulltag.pk3 ..\output\RGH-ZDoom.pk3
-cd ..\temp\acs-skulltag
-..\..\utility\7z a ..\..\output\RGH-Skulltag.pk3 acs\RGH_ACS -mx0
-cd ..\acs-zdoom
-..\..\utility\7z a ..\..\output\RGH-ZDoom.pk3 acs\RGH_ACS -mx0
-cd "..\..\zdoom data"
-..\utility\7z u ..\output\RGH-ZDoom.pk3 * -xr!.svn -mx0
-cd ..
-exit
-
-:acserror
-	echo ==============================================================
-	move temp\acs-skulltag\acs.err acs_errors_skulltag.log
-	move temp\acs-zdoom\acs.err acs_errors_zdoom.log	
-	pause
+        cd data
+        ..\utility\7z a ..\output\RGH-Skulltag.pk3 * -xr!.svn -mx0
+        copy ..\output\RGH-Skulltag.pk3 ..\output\RGH-ZDoom.pk3
+        cd ..\temp\acs-skulltag
+        ..\..\utility\7z a ..\..\output\RGH-Skulltag.pk3 acs\RGH_ACS -mx0
+        cd ..\acs-zdoom
+        ..\..\utility\7z a ..\..\output\RGH-ZDoom.pk3 acs\RGH_ACS -mx0
+        cd "..\..\zdoom data"
+        ..\utility\7z u ..\output\RGH-ZDoom.pk3 * -xr!.svn -mx0
+        cd ..
+        exit
+    )
+)
